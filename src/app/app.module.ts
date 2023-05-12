518845e370dfdd0235aa1f00c47ff2f43866b6dc
Antoine Plantec <antoine.plantec@ecam-strasbourg.eu>
2023-05-12 15:57:36 +0200

    page gestion de stock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageLoginComponent } from './pages/page-login/page-login.component';
 import { PageInscriptionComponent } from './pages/page-inscription/page-inscription.component';
+import { PageDashboardComponent } from './pages/page-dashboard/page-dashboard.component';
+import { PageStatistiquesComponent } from './pages/page-statistiques/page-statistiques.component';
+import { MenuComponent } from './composants/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageLoginComponent,
     PageInscriptionComponent,
+    PageDashboardComponent,
+    PageStatistiquesComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
